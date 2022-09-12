9705fc486cc2ae3b811a10aa33ee4ada8b7fbc04
Luiz Gustavo Chaves dias <111753842+dias0001@users.noreply.github.com>
2022-09-12 11:34:32 -0300

    processo 1

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,47 @@
+
+/* ///////////// funções /////////////////////////////////////////// */
+const aviso = (msg) => {
+    alert(msg)
+}
+
+
+
+
+
+/* ----------------------------funções------------------------------------- */
+
+
+
+
+
+
+/* //////////////////////// eventos e execuções automaticas /////  */
+
+//document.getElementById('nome').addEventListener('click', aviso)
+aviso('Tenha um bom dia Dave')
+
+
+
+
 //inicializada animações scrool do AOS
 AOS.init();
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
-  
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-  
+
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
     })
-  })()
+})()
