e28ef43fafa85f17f24b24bc73f7b97223123716
Luiz Gustavo Chaves dias <111753842+dias0001@users.noreply.github.com>
2022-09-13 11:36:34 -0300

    processo 13-09-22

--- js/custom.js
+++ js/custom.js
@@ -7,18 +7,33 @@ const aviso = (msg) => {
 const mostraidade = () => {
     let span = document.getElementById('txt-idade')
     let campoidade = document.getElementById('idade')
-    span.innerText = campoidade.value    
+    span.innerText = campoidade.value
 }
 
+var dataAtual = new Date()
+const mostraData = () => {
+    let dia = dataAtual.getDay()
+    let mes = dataAtual.getMonth() + 1
+    let ano = dataAtual.getFullYear()
+    let hora = dataAtual.getHours()
+    let valor = dia + '/' + mes + '/' + ano + '-' + hora
+    document.getElementById('dt-cadastro').value = valor
+}
+
+//Preenche o select "estado" com os estados da API do IBGE
+const getEstados = () => {
+    let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+    let select = document.getElementById(estado)
+
+    // Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
+    fetch(api).then(resposta => resposta.json()).then(json => {
+        console.log(json)
+    })
 
 
+}
 
-
-/* ----------------------------funções------------------------------------- */
-
-
-
-
+/* ---------------------------------------------------------------- */
 
 
 /* //////////////////////// eventos e execuções automaticas /////  */
@@ -27,10 +42,19 @@ const mostraidade = () => {
 aviso('Tenha um bom dia Dave')
 document.getElementById('nome').addEventListener('click' , function(){
     aviso('olá mundo !!! Este é o planeta terra')
-}) */ 
+}) */
 
 mostraidade()
 document.getElementById('idade').addEventListener('change', mostraidade)
+
+mostraData()
+
+getEstados()
+
+
+
+
+
 
 
 
@@ -39,16 +63,21 @@ document.getElementById('idade').addEventListener('change', mostraidade)
 //inicializada animações scrool do AOS
 AOS.init();
 
+// Impede o envio do formulário quando os campos estão inválidos
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    //váriavel captura as tags <form> que contém a classe "nedds-validation"    
     const forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
+
+
+    // Executa para cada formulário da variável forms
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
+            //Se houver campos inválidos, interrompe o SUBMIT
             if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
